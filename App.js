d13ef1db2f7501bcb3433b2de57321c998fbab2e
anshutoppo <59956938+anshutoppo@users.noreply.github.com>
2022-03-24 14:01:07 +0530

    Added files

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MemeGenerator from './Component/MemeGenerator';
+
+function App() {
+  return (
+    <div className="App">
+     <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
